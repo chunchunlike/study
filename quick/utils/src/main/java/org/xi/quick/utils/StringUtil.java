bfa9a6fcf7467003011b4054da1af50e704d604b
Xi Xi <rarexixi@gmail.com>
2017-05-17 15:11:04 +0800

    字符串工具类添加邮箱验证

--- quick/utils/src/main/java/org/xi/quick/utils/StringUtil.java
+++ quick/utils/src/main/java/org/xi/quick/utils/StringUtil.java
@@ -1,5 +1,8 @@
 package org.xi.quick.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Xi on 5/17/2017.
  */
@@ -23,5 +26,18 @@ public class StringUtil {
      */
     public static boolean isNullOrWhiteSpace(String s) {
         return s == null || s.trim().isEmpty();
+    }
+
+
+    /**
+     * 是否是邮箱
+     *
+     * @param s 输入字符串
+     * @return
+     */
+    public static boolean isEmail(String s) {
+        Pattern pattern = Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(s);
+        return matcher.matches();
     }
 }
