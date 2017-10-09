e6bc2749fb11672cc39133421afa3725ec4dff79
郗世豪 <rarexixi@sina.com>
2017-10-09 13:57:20 +0800

    spring cloud 学习 断路器Hystrix修改

---
+++ quick/springcloud/spring-cloud-routing-zuul/src/main/java/org/xi/springcloud/routingzuul/RoutingZuulApplication.java
@@ -0,0 +1,12 @@
+package org.xi.springcloud.routingzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingZuulApplication.class, args);
+	}
+}
