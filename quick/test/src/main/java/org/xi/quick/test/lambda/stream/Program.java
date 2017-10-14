bfd4e3d5cc87952c8fc89abcb4174c300f2978c9
郗世豪 <rarexixi@sina.com>
2017-10-14 16:14:04 +0800

    Java Stream collector 测试

---
+++ quick/test/src/main/java/org/xi/quick/test/lambda/stream/Program.java
@@ -0,0 +1,138 @@
+package org.xi.quick.test.lambda.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    public static void main(String[] args) {
+        printSplitLine("toLookupTest");
+        toMapTest();
+
+        printSplitLine("groupByTest");
+        groupByTest();
+
+        printSplitLine("partitionTest");
+        partitionTest();
+
+        printSplitLine("selectTest");
+        selectTest();
+
+        printSplitLine();
+
+        printSplitLine();
+
+        printSplitLine();
+
+        printSplitLine();
+
+        printSplitLine();
+
+    }
+
+    static void toMapTest() {
+
+        List<User> userList = getUserList();
+
+        Map<Integer, User> userMap =
+                userList.stream()
+                        .collect(Collectors.toMap(user -> user.getId(), user -> user));
+        System.out.println(userMap);
+
+        //Map<Integer, User> userMap2 =
+        userList.stream().collect(Collectors.toMap(user -> user.getId(), user -> user));
+        System.out.println(userMap);
+    }
+
+    static void groupByTest() {
+
+        List<User> userList = getUserList();
+
+        Map<String, List<User>> userMap =
+                userList.stream()
+                        .collect(Collectors.groupingBy(user -> user.getSex()));
+        System.out.println(userMap);
+
+        Map<String, Long> userMap2 =
+                userList.stream()
+                        .collect(Collectors.groupingBy(user -> user.getSex(),
+                                Collectors.counting()));
+        System.out.println(userMap2);
+
+        Map<String, Map<String, List<User>>> userMap3 =
+                userList.stream()
+                        .collect(Collectors.groupingBy(user -> user.getSex(),
+                                Collectors.groupingBy(user -> {
+                                    if (user.getWeight() > 80) return "超重";
+                                    if (user.getWeight() > 60) return "正常";
+                                    return "过轻";
+                                })));
+        System.out.println(userMap3);
+
+        Map<String, User> userMap4 =
+                userList.stream()
+                        .collect(Collectors.groupingBy(user -> user.getSex(),
+                                Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingDouble(user -> user.getWeight())),
+                                        Optional::get)));
+        System.out.println(userMap4);
+
+        Map<String, List<User>> userMap5 =
+                userList.stream()
+                        .collect(Collectors.groupingBy(user -> user.getSex()));
+        userMap5.forEach((key, entry) -> {
+            entry.sort(Comparator.comparing(user -> user.getWeight()));
+        });
+        System.out.println(userMap5);
+    }
+
+    static void partitionTest() {
+
+        List<User> userList = getUserList();
+
+        Map<Boolean, List<User>> userPartition =
+                userList.stream()
+                        .collect(Collectors.partitioningBy(user -> user.getSex().equals("男")));
+        System.out.println(userPartition);
+
+        Map<Boolean, Long> userPartition2 =
+                userList.stream()
+                        .collect(Collectors.partitioningBy(user -> user.getSex().equals("男"),
+                                Collectors.counting()));
+        System.out.println(userPartition2);
+    }
+
+    static List<User> getUserList() {
+        List<User> userList = Arrays.asList(
+                new User(1, "郗世豪", "男", 76.5),
+                new User(2, "朱凡凡", "女", 50.5),
+                new User(3, "胡永强", "男", 80.8),
+                new User(4, "贺嘉良", "男", 80.5),
+                new User(5, "张敏", "男", 56.5),
+                new User(6, "曹青", "女", 53.5)
+        );
+        return userList;
+    }
+
+    static void selectTest() {
+        List<Integer> numbers1 = Arrays.asList(1, 2, 5, 3);
+        List<Integer> numbers2 = Arrays.asList(21, 42, 56, 32, 54, 625);
+        List<List<Integer>> pairs =
+                numbers1.stream()
+                        .flatMap(i ->
+                                numbers2.stream()
+                                        .filter(j -> i * j % 5 == 0)
+                                        .map(j -> Arrays.asList(i, j)))
+                        .collect(Collectors.toList());
+
+        System.out.println(pairs);
+    }
+
+    static void printSplitLine() {
+        printSplitLine("");
+    }
+
+    static void printSplitLine(String s) {
+        System.out.println();
+        System.out.println("=================================" + s + "================================");
+    }
+}
---
+++ quick/test/src/main/java/org/xi/quick/test/lambda/stream/User.java
@@ -0,0 +1,56 @@
+package org.xi.quick.test.lambda.stream;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String sex;
+    private Double weight;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String sex, Double weight) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.weight = weight;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "[id:" + id + ", name:" + name + ", sex:" + sex + ", weight: " + weight + "]";
+    }
+}
